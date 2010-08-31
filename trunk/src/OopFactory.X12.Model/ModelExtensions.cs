374c9127dd7e29e555461cff22eb86a668a33806
dstrubhar_cp <SND\dstrubhar_cp>
2010-08-31 01:29:24 +0000

    Start implementing model for claim.

---
+++ trunk/src/OopFactory.X12.Model/ModelExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace OopFactory.X12.Model
+{
+    public static class ModelExtensions
+    {
+        public static TModel Deserialize<TModel>(string xml)
+        {
+            var serializer = new XmlSerializer(typeof(TModel));
+
+            MemoryStream ms = new MemoryStream();
+            XmlTextWriter writer = new XmlTextWriter(ms, Encoding.UTF8);
+            return (TModel)serializer.Deserialize(ms);
+        }
+
+
+    }
+}
