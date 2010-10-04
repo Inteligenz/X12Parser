eed724302d1946ce13034d2f95bbfcdf8b239dfa
dstrubhar_cp <SND\dstrubhar_cp>
2010-10-04 20:17:54 +0000

    Add more details to the Claim model. Add testing of Institutional claim loops.

--- src/OopFactory.X12.Model/Claims/Claim.cs
+++ src/OopFactory.X12.Model/Claims/Claim.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace OopFactory.X12.Model.Claims
 {
     public class Claim
     {
+        [XmlElement(ElementName = "Identification")]
+        public List<QualifiedNumber> Identifications { get; set; }
+
         public Subscriber Subscriber { get; set; }
+
+        [XmlElement(ElementName = "Provider")]
+        public List<Provider> Providers { get; set; }
+
+        [XmlElement(ElementName = "ServiceLine")]
+        public List<ServiceLine> ServiceLines { get; set; }        
     }
 }
--- src/OopFactory.X12.Model/EntityName.cs
+++ src/OopFactory.X12.Model/EntityName.cs
@@ -9,9 +9,33 @@ namespace OopFactory.X12.Model
     public class EntityName
     {
         [XmlAttribute]
-        public bool IsPerson { get; set; }
+        public EntityTypeQualifierEnum Qualifier { get; set; }
+
+        [XmlAttribute]
+        public string Prefix { get; set; }
+        [XmlAttribute]
+        public string Suffix { get; set; }
+
         public string First { get; set; }
         public string Middle { get; set; }
         public string Last { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                if (Qualifier == EntityTypeQualifierEnum.NonPersonEntity)
+                    return Last;
+                else
+                {
+                    var name = String.Format("{0} {1}, {2} {3} {4}", Last, Suffix, Prefix, First, Middle);
+                    name = name.Replace("  ", " ");
+                    name = name.Replace("  ", " ");
+                    name = name.Replace(" ,", ",");
+
+                    return name.Trim();
+                }
+            }
+        }
     }
 }
