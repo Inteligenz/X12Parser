200a6f42d5f34f520fe169a43b5ad51f27c438a9
dstrubhar_cp <SND\dstrubhar_cp>
2010-10-07 14:10:12 +0000

    Add more adjustment content to xslt for 835.

---
+++ trunk/src/OopFactory.X12.Model/QualifiedAmount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OopFactory.X12.Model
+{
+    public class QualifiedAmount
+    {
+        public CodedLookup Qualifier { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
