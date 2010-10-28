c0b7ca673db42565022fa1615a0bf9d27774b923
dstrubhar_cp <SND\dstrubhar_cp>
2010-10-28 15:05:34 +0000

    Add institutional codes.

--- src/OopFactory.X12.Model/Claims/Claim.cs
+++ src/OopFactory.X12.Model/Claims/Claim.cs
@@ -8,13 +8,30 @@ namespace OopFactory.X12.Model.Claims
 {
     public class Claim
     {
+        [XmlAttribute]
+        public ClaimTypeEnum Type { get; set; }
+
+        [XmlAttribute]
+        public string PatientControlNumber { get; set; }
+
+        [XmlAttribute]
+        public decimal TotalCharges { get; set; }
+
         [XmlElement(ElementName = "Identification")]
         public List<QualifiedNumber> Identifications { get; set; }
 
         public Subscriber Subscriber { get; set; }
 
+        public Member Patient { get; set; }
+
         [XmlElement(ElementName = "Provider")]
         public List<Provider> Providers { get; set; }
+
+        [XmlElement(ElementName = "Occurrence")]
+        public List<Occurrence> Occurrences { get; set; }
+
+        [XmlElement(ElementName = "OccurrenceSpan")]
+        public List<OccurrenceSpan> OccurrenceSpans { get; set; }
 
         [XmlElement(ElementName = "ServiceLine")]
         public List<ServiceLine> ServiceLines { get; set; }        
---
+++ src/OopFactory.X12.Model/Claims/ClaimTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OopFactory.X12.Model.Claims
+{
+    public enum ClaimTypeEnum
+    {
+        Dental,
+        Institutional,
+        Professional
+    }
+}
