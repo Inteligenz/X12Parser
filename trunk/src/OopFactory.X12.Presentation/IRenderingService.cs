38f9db5741f076bdc4d468dd4dde0b461aeea234
dstrubhar_cp <SND\dstrubhar_cp>
2011-03-21 02:26:35 +0000

    Refactor transformation to pdf so that xslt can be reused and projections can be defined in C#.

---
+++ trunk/src/OopFactory.X12.Presentation/IRenderingService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OopFactory.X12.Presentation
+{
+    public interface IRenderingService
+    {
+        byte[] CreatePdf(string domainXml);
+    }
+}
---
+++ trunk/src/OopFactory.X12.Presentation/Model/FoPageSequence.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace OopFactory.X12.Presentation.Model
+{
+    public class FoPageSequence
+    {
+        public FoPageSequence()
+        {
+            if (Fields == null) Fields = new List<FoPageField>();
+        }
+        [XmlAttribute]
+        public string MasterReference { get; set; }
+
+        [XmlElement(ElementName="Field")]
+        public List<FoPageField> Fields { get; set; }
+
+        public FoPageField AddField(string content, decimal top, decimal left, decimal width)
+        {
+            var field = new FoPageField
+            {
+                Content = content,
+                Top = top,
+                Left = left,
+                Width = width,
+                Height = 1
+            };
+            Fields.Add(field);
+            return field;
+        }
+    }
+}
