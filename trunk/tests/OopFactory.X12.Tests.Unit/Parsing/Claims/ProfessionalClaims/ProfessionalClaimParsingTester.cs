92d4b9120b76a1ae84368edf19ca4fd7515bef87
dstrubhar_cp <SND\dstrubhar_cp>
2011-04-07 00:54:57 +0000

    Add support for 837P 5010

---
+++ trunk/tests/OopFactory.X12.Tests.Unit/Parsing/Claims/ProfessionalClaims/ProfessionalClaimParsingTester.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+using OopFactory.X12.Parsing;
+using OopFactory.X12.Parsing.Model;
+
+namespace OopFactory.X12.Tests.Unit.Parsing.Claims.ProfessionalClaims
+{
+    [TestClass]
+    public class ProfessionalClaimParsingTester
+    {
+        private Stream GetEdi(string filename)
+        {
+            return Assembly.GetExecutingAssembly().GetManifestResourceStream("OopFactory.X12.Tests.Unit.Parsing.Claims.ProfessionalClaims." + filename);
+        }
+
+        [TestMethod]
+        public void ParseSample1ToXml()
+        {
+            X12Parser parser = new X12Parser();
+            Interchange interchange = parser.Parse(GetEdi("Sample1_5010.txt"));
+            string xml = interchange.Serialize();
+            Trace.Write(xml);
+        }
+    }
+}
