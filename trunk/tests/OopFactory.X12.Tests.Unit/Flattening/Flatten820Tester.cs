2e902102e1404af899148cc8529ac34f053482db
dstrubhar_cp <SND\dstrubhar_cp>
2011-05-06 03:14:13 +0000

    Added unit test for flattening an 820.

---
+++ trunk/tests/OopFactory.X12.Tests.Unit/Flattening/Flatten820Tester.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+using System.IO;
+using OopFactory.X12.Parsing;
+using OopFactory.X12.Parsing.Model;
+using System.Xml;
+
+namespace OopFactory.X12.Tests.Unit.Flattening
+{
+    [TestClass]
+    public class Flatten820Tester
+    {
+        [TestMethod]
+        public void FlattenUsingXmlDocument()
+        {
+            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("OopFactory.X12.Tests.Unit.Parsing._SampleEdiFiles.ORD._820.Example1_MortgageBankers.txt");
+
+            X12Parser parser = new X12Parser();
+            Interchange interchange = parser.Parse(stream);
+            string xml = interchange.Serialize();
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+
+            FileStream fstream = new FileStream("c:\\Temp\\ORD._820.Example1.txt", FileMode.Create);
+            StreamWriter writer = new StreamWriter(fstream);
+
+            writer.WriteLine("Transaction,Creation Date,Submitter Name, Borrower Last Name, Remittance ID");
+            foreach (XmlElement transaction in doc.SelectNodes("/Interchange/FunctionGroup/Transaction"))
+            {
+                foreach (XmlElement entity in transaction.SelectNodes("Loop[@LoopId='ENT']"))
+                {
+                    foreach (XmlElement remit in entity.SelectNodes("Loop[@LoopId='RMR']"))
+                    {
+                        writer.WriteLine("{0},{1},{2},{3},{4}",
+                            transaction.SelectSingleNode("ST/ST02").InnerText,
+                            transaction.SelectSingleNode("DTM[DTM01='097']/DTM02").InnerText,
+                            transaction.SelectSingleNode("Loop[@LoopId='N1']/N1[N101='41']/N102").InnerText,
+                            entity.SelectSingleNode("Loop[@LoopId='NM1']/NM1[NM101='BW']/NM103").InnerText,
+                            remit.SelectSingleNode("RMR/RMR02").InnerText);
+                    }
+                }
+            }
+            
+            writer.Close();
+            fstream.Close();
+            
+        }
+    }
+}
