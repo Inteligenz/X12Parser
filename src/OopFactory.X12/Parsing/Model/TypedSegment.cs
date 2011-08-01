ca327911a70f12597b2aeea1da405aef7ad30a09
dstrubhar_cp <SND\dstrubhar_cp>
2011-08-01 01:27:29 +0000

    Added typed segment for DMG, PAT, PRV and SBR for creating 837 messages.

--- src/OopFactory.X12/Parsing/Model/TypedSegment.cs
+++ src/OopFactory.X12/Parsing/Model/TypedSegment.cs
@@ -15,9 +15,25 @@ namespace OopFactory.X12.Parsing.Model
             _segmentId = segmentId;
         }
 
+        public event EventHandler Initializing;
+        public event EventHandler Initialized;
+
+        protected virtual void OnInitializing(EventArgs e)
+        {
+            if (Initializing != null)
+                Initializing(this, e);
+        }
+        protected virtual void OnInitialized(EventArgs e)
+        {
+            if (Initialized != null)
+                Initialized(this, e);
+        }
+
         internal void Initialize(Container parent, X12DelimiterSet delimiters)
         {
+            OnInitializing(new EventArgs());
             _segment = new Segment(parent, delimiters, _segmentId);
+            OnInitialized(new EventArgs());
         }
     }
 }
