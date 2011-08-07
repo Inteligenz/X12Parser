85df56effd598e4b25e717116374938fa5b00e43
dstrubhar_cp <SND\dstrubhar_cp>
2011-08-07 14:18:37 +0000

    Added CLM and SBR typed loops and corrected HI segment specification. Added more segments to unit test to resolve issue reported in discussion: http://x12parser.codeplex.com/discussions/265463.

---
+++ src/OopFactory.X12/Parsing/Model/Typed/TypedElementHealthCareCodeInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OopFactory.X12.Parsing.Model.Typed
+{
+    public class TypedElementHealthCareCodeInfo
+    {
+        private int _elementNumber;
+        private Segment _segment;
+
+        internal TypedElementHealthCareCodeInfo(Segment segment, int elementNumber)
+        {
+            _segment = segment;
+            _elementNumber = elementNumber;
+        }
+
+        private void UpdateElement()
+        {
+        }
+        /*
+        public string _1_CodeListQualifierCode
+        {
+        }
+
+        public string _2_IndustryCode
+        {
+        }
+         * */
+    }
+}
