80044eeb1ad0359654cda620897be97cda1603e9
traxine_cp <SND\traxine_cp>
2011-08-12 18:20:06 +0000

    First draft! Refinements to come.

---
+++ src/OopFactory.X12/Parsing/Model/Typed/TypedSegmentDTP.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OopFactory.X12.Parsing.Model.Typed
+{
+    public class TypedSegmentDTP : TypedSegment
+    {
+        public TypedSegmentDTP() : base("DTP"){}
+
+        public string DTP01_DateTimeQualifier
+        {
+            get { return _segment.GetElement(1); }
+            set { _segment.SetElement(1, value); }       
+        }
+
+        public string DTP02_DateTimePeriodFormatQualifier
+        {
+            get { return _segment.GetElement(2); }
+            set { _segment.SetElement(2, value); }              
+        }
+
+        public DateTime? DTP03_Date
+        {
+            get
+            {
+                string element = _segment.GetElement(3);
+                if (element.Length == 8)
+                    return DateTime.ParseExact(element, "yyyyMMdd", null);
+                return null;
+            }
+            set
+            {
+                _segment.SetElement(3, String.Format("{0:yyyyMMdd}", value));
+            }
+        }
+
+    }
+
+    
+
+}
