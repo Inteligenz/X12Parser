3e6b7016b6393684841ecd3fa1b96bab662c5b13
dstrubhar_cp <SND\dstrubhar_cp>
2011-08-27 15:27:47 +0000

    Start modeling Eligibility Inquiry, fixed UB04Claim so that it would compile.

---
+++ src/OopFactory.X12.Hipaa/EntityName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OopFactory.X12.Hipaa
+{
+    public class EntityName
+    {
+        public string Identifier { get; set; }
+        public string Qualifier { get; set; }
+        public string Prefix { get; set; }
+        public string Suffix { get; set; }
+
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+
+    }
+}
