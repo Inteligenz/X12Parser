27f415c88894631ff6cdbfae71f285f7f4fdb61e
dstrubhar_cp <SND\dstrubhar_cp>
2011-08-29 11:44:54 +0000

    Start modeling 276/277.

---
+++ trunk/src/OopFactory.X12.Hipaa/ClaimStatus/ClaimStatusRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OopFactory.X12.Hipaa.Common;
+
+namespace OopFactory.X12.Hipaa.ClaimStatus
+{
+    public class ClaimStatusRequest : ClaimStatusBase
+    {
+
+    }
+}
---
+++ trunk/src/OopFactory.X12.Hipaa/ClaimStatus/ClaimStatusServiceLineResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using OopFactory.X12.Hipaa.Common;
+
+namespace OopFactory.X12.Hipaa.ClaimStatus
+{
+    public class ClaimStatusServiceLineResponse
+    {
+        public DateTime BeginDate { get; set; }
+    }
+}
