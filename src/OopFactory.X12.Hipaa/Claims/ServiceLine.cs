c0a74a16a144b7f06b7f93980ecb5b71e9152f87
dstrubhar_cp <SND\dstrubhar_cp>
2011-08-29 13:51:37 +0000

    Building out claim model.

---
+++ src/OopFactory.X12.Hipaa/Claims/ServiceLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using OopFactory.X12.Hipaa.Common;
+
+namespace OopFactory.X12.Hipaa.Claims
+{
+    public class ServiceLine
+    {
+        public ServiceLine()
+        {
+            if (Identifications == null) Identifications = new List<Identification>();
+            if (Amounts == null) Amounts = new List<QualifiedAmount>();
+            if (Dates == null) Dates = new List<QualifiedDate>();
+            if (DateRanges == null) DateRanges = new List<QualifiedDateRange>();
+            if (Notes == null) Notes = new List<string>();
+        }
+
+        [XmlAttribute]
+        public int LineNumber { get; set; }
+
+        public MedicalProcedure Procedure { get; set; }
+        public List<Identification> Identifications { get; set; }
+        public List<QualifiedAmount> Amounts { get; set; }
+        public List<QualifiedDate> Dates { get; set; }
+        public List<QualifiedDateRange> DateRanges { get; set; }
+        public List<string> Notes { get; set; }
+    }
+}
---
+++ src/OopFactory.X12.Hipaa/Common/CodedDateRange.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace OopFactory.X12.Hipaa.Common
+{
+    public class CodedDateRange
+    {
+        [XmlAttribute]
+        public string Code { get; set; }
+        [XmlAttribute(DataType = "date")]
+        public DateTime FromDate { get; set; }
+        [XmlAttribute(DataType = "date")]
+        public DateTime ThroughDate { get; set; }
+    }
+}
