a9e8d25bf055f023e9a69c8902b38ad8d3f98c3f
epkrause_cp <SND\epkrause_cp>
2011-09-20 15:52:55 +0000

    Added tests

--- trunk/tests/OopFactory.X12.Hipaa.Tests.Unit/Claims/X12ToHCFADataModelTester.cs
+++ trunk/tests/OopFactory.X12.Hipaa.Tests.Unit/Claims/X12ToHCFADataModelTester.cs
@@ -37,7 +37,28 @@ namespace OopFactory.X12.Hipaa.Tests.Unit.Claims
             //var document = claimSvc.Transform837ToClaimDocument(stream);
             //var claim = document.Claims[0];
             //Assert.AreEqual("587654321", claim.BillingInfo.Providers[0].TaxId);
-            var hcfaclaim = claimSvc.TransformX12837ToHCFA1500Model(stream);
+            var hcfaclaim = claimSvc.TransformX12837ToHcfa1500Model(stream);
+            Assert.AreEqual("JANE", hcfaclaim.Field02_PatientsFirstName);
+            Assert.AreEqual("SMITH", hcfaclaim.Field02_PatientsLastName);
+            Assert.IsFalse(hcfaclaim.Field01_TypeOfCoverageIsMedicare);
+            Assert.IsFalse(hcfaclaim.Field01_TypeOfCoverageIsMedicaid);
+            //Assert.AreEqual("1943-05-01T00:00:00", hcfaclaim.Field03_PatientsDateOfBirth);
+            if (hcfaclaim.Field03_PatientsSexFemale.HasValue)
+            {
+                Assert.IsTrue(hcfaclaim.Field03_PatientsSexFemale.Value);
+            }
+            Assert.AreEqual("99213", hcfaclaim.Field24_ServiceLines.First().Field24d_ProcedureCode);
+            Assert.AreEqual("87070", hcfaclaim.Field24_ServiceLines[1].Field24d_ProcedureCode);
+            Assert.AreEqual("99214", hcfaclaim.Field24_ServiceLines[2].Field24d_ProcedureCode);
+            Assert.AreEqual("86663", hcfaclaim.Field24_ServiceLines[3].Field24d_ProcedureCode);
+                             Assert.AreEqual("BEN KILDARE SERVICE", hcfaclaim.Field32_FacilityLocationInfo_Name);
+            Assert.AreEqual("234 SEAWAY ST", hcfaclaim.Field32_FacilityLocationInfo_Street);
+            Assert.AreEqual("MIAMI", hcfaclaim.Field32_FacilityLocationInfo_City);
+            Assert.AreEqual("FL", hcfaclaim.Field32_FacilityLocationInfo_State);
+            Assert.AreEqual("2345 OCEAN BLVD", hcfaclaim.Field33_BillingProvider_Street);
+            Assert.AreEqual("MAIMI", hcfaclaim.Field33_BillingProvider_City);
+            Assert.AreEqual("FL", hcfaclaim.Field33_BillingProvider_State);
+            Assert.AreEqual("33111", hcfaclaim.Field33_BillingProvider_Zip);
             Trace.Write(hcfaclaim.Serialize());
         }
 #endif
