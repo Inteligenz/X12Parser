7edd6c603893aa6046aee229f156aa01c847ef33
dstrubhar_cp <SND\dstrubhar_cp>
2012-07-09 14:39:28 +0000

    Change Serialize to serialize directly to a file instead of to memory.

--- src/OopFactory.X12.X12Parser/Program.cs
+++ src/OopFactory.X12.X12Parser/Program.cs
@@ -21,24 +21,17 @@ namespace OopFactory.X12.X12Parser
             fs.Close();
             if (interchanges.Count >= 1)
             {
-                string xml = interchanges.First().Serialize();
-
                 FileStream outputFs = new FileStream(outputFilename, FileMode.Create);
-                StreamWriter writer = new StreamWriter(outputFs);
-                writer.Write(xml);
-                writer.Close();
+                interchanges.First().Serialize(outputFs);
+                outputFs.Close();
             }
             if (interchanges.Count > 1)
             {
                 for (int i = 1; i < interchanges.Count; i++)
                 {
-                    string xml = interchanges[i].Serialize();
-
                     outputFilename = string.Format("{0}_{1}.xml", args.Length > 1 ? args[1] : x12Filename, i + 1);
                     FileStream outputFs = new FileStream(outputFilename, FileMode.Create);
-                    StreamWriter writer = new StreamWriter(outputFs);
-                    writer.Write(xml);
-                    writer.Close();
+                    interchanges[i].Serialize(outputFs);
 
                 }
             }
