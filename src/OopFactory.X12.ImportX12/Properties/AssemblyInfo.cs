c071413dba3097620d054a4458b2d999e1e6bc12
dstrubhar_cp <SND\dstrubhar_cp>
2013-04-10 00:42:51 +0000

    Added ImportX12 console app for importing x12 into a SQL Server database.

--- src/OopFactory.X12.ImportX12/Properties/AssemblyInfo.cs
+++ src/OopFactory.X12.ImportX12/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OopFactory.X12.Hipaa.ClaimParser")]
+[assembly: AssemblyTitle("OopFactory.X12.ImportX12")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("OopFactory.X12.Hipaa.ClaimParser")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OopFactory.X12.ImportX12")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6dc65d4b-7f73-485a-97f1-fd69c061d817")]
+[assembly: Guid("65defb8b-f296-49d2-8f38-b39ddeda27e8")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
