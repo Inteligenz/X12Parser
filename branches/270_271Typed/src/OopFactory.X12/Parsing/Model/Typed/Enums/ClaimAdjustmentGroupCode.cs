91b8aacd9870b8805ba0e39e2d06b1a82d0e6791
pcalchi_cp <SND\pcalchi_cp>
2015-07-04 17:40:54 +0000

    Last batch of typed 835 changes
    *Bug fix for SVC loop
    *Addition of NM1 segment
    * Addition of Claim Adjustmentgroupcode enum
    * An integration test illustrating how to parse an 835 file in a strongly typed manner.

---
+++ branches/270_271Typed/src/OopFactory.X12/Parsing/Model/Typed/Enums/ClaimAdjustmentGroupCode.cs
@@ -0,0 +1,22 @@
+﻿using OopFactory.X12.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OopFactory.X12.Parsing.Model.Typed.Enums
+{
+    public enum ClaimAdjustmentGroupCodes
+    {
+        [EDIFieldValue("CO")]
+        ContractualObligations,
+        [EDIFieldValue("CR")]
+        CorrectionAndReversals,
+        [EDIFieldValue("OA")]
+        OtherAdjustments,
+        [EDIFieldValue("PI")]
+        PayorInitiatedReductions,
+        [EDIFieldValue("PR")]
+        PatientResponsibility,
+    }
+}
