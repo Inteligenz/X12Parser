0a9ea0cc9f0aaab371f390d69de1b146d9d81dc5
pcalchi_cp <SND\pcalchi_cp>
2015-11-14 04:11:18 +0000

    Updating AMT and LX  typed segments

---
+++ src/OopFactory.X12/Parsing/Model/Typed/Enums/AmountQualifierCode.cs
@@ -0,0 +1,39 @@
+﻿using OopFactory.X12.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+
+namespace OopFactory.X12.Parsing.Model.Typed.Enums
+{
+    public enum AmountQualifierCode
+    {
+        [EDIFieldValue("F5")]
+        PatientAmountPaid,
+
+        [EDIFieldValue("D")]
+        PayorAmountPaid,
+
+        [EDIFieldValue("A8")]
+        NoncoveredCharges_Actual,
+
+        [EDIFieldValue("EAF")]
+        AmountOwed,
+
+        [EDIFieldValue("T")]
+        Tax,
+
+        [EDIFieldValue("F4")]
+        PostageClaimed,
+
+        [EDIFieldValue("B6")]
+        AllowedAmount_Actual,
+
+        [EDIFieldValue("T3")]
+        TotalSubmittedCharges,
+
+        [EDIFieldValue("N1")]
+        NetWorth,
+    }
+}
--- src/OopFactory.X12/Parsing/Model/Typed/TypedLoopLX.cs
+++ src/OopFactory.X12/Parsing/Model/Typed/TypedLoopLX.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace OopFactory.X12.Parsing.Model.Typed
+﻿namespace OopFactory.X12.Parsing.Model.Typed
 {
     public class TypedLoopLX : TypedLoop
     {
-        private string _entityIdentifer;
-
-        public TypedLoopLX(string entityIdentifier) 
+        public TypedLoopLX() 
             : base("LX")
         {
-            _entityIdentifer = entityIdentifier;
         }
 
         public TypedLoopLX(Loop loop) : base(loop) { }
@@ -24,9 +16,9 @@ namespace OopFactory.X12.Parsing.Model.Typed
             Loop = new Loop(parent, delimiters, segmentString, loopSpecification);
         }
 
-        public string LX01_AssignedNumber
+        public int? LX01_AssignedNumber
         {
-            get { return Loop.GetElement(1); }
+            get { return Loop.GetIntElement(1); }
             set { Loop.SetElement(1, value); }
         }
     }
