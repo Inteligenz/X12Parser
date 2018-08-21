212ef7cfa6310550e964706ebd56ca6e53ae283f
Bradley Van Fleet <bradley.vanfleet@healthcatalyst.com>
2018-08-21 11:43:48 -0600

    Updated reference to no long user obselete X12.Repositories code

--- src/OopFactory.X12.Hipaa/Claims/Repositories/SqlClaimRepository.cs
+++ src/OopFactory.X12.Hipaa/Claims/Repositories/SqlClaimRepository.cs
@@ -1,13 +1,13 @@
 ﻿namespace OopFactory.X12.Hipaa.Claims.Repositories
 {
-    using OopFactory.X12.Repositories;
     using OopFactory.X12.Specifications.Finders;
+    using OopFactory.X12.Sql;
 
     /// <summary>
     /// Represents a repository for claims data
     /// </summary>
     /// <typeparam name="T">Claim type stored by the repository</typeparam>
-    public class SqlClaimRepository<T> : SqlTransactionRepository<T> where T : struct
+    public class SqlClaimRepository<T> : SqlTransactionRepository
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="SqlClaimRepository{T}"/> class
@@ -19,6 +19,7 @@
                 dsn,
                 new SpecificationFinder(),
                 "AMT,BHT,CAS,CL1,CLM,CN1,CR1,CR2,CR3,CR4,CR5,CR6,CR7,CR8,CRC,CTP,CUR,DMG,DN1,DN2,DSB,DTP,HCP,HI,HL,HSD,IMM,K3,LIN,LX,MEA,MIA,MOA,N2,N3,N4,NM1,NTE,OI,PAT,PER,PRV,PS1,PWK,QTY,REF,SBR,SE,ST,SV1,SV2,SV3,SV4,SV5,SV6,SV7,SVD,TOO,UR".Split(','),
+                typeof(T),
                 schema)
         {
         }
