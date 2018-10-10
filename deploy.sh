be305d294d8df747f0c3a97bc92fbdb5cace6777
Bradley Van Fleet <bradley.vanfleet@healthcatalyst.com>
2018-10-10 09:30:30 -0600

    Manually filled in nuspec files, since Travis seems to be having issues with auto-completing them. Updated the deploy.sh script to follow a set pattern of packing.

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,20 @@
 ApiKey=$1
 Source=$2
+
+# Defining the version here for consistent versioning across packages, may segregate later on
 Version="5.0.${3}"
 
-# List all packages with nuspecs to be packed and pushed
-nuget pack ./src/X12.Hipaa/package.nuspec -Version $Version -Verbosity detailed
-nuget pack ./src/X12.Parsing/package.nuspec -Version $Version -Verbosity detailed
-nuget pack ./src/X12.Shared/package.nuspec -Version $Version -Verbosity detailed
+# Core packages that need to be packed prior to any other packages
 nuget pack ./src/X12.Specifications/package.nuspec -Version $Version -Verbosity detailed
-nuget pack ./src/X12.Sql/package.nuspec -Version $Version -Verbosity detailed
+nuget pack ./src/X12.Shared/package.nuspec -Version $Version -Verbosity detailed
+nuget pack ./src/X12.Transformations/package.nuspec -Version $Version -Verbosity detailed
+nuget pack ./src/X12.Parsing/package.nuspec -Version $Version -Verbosity detailed
+
+# Supplemental packages to pack
 nuget pack ./src/X12.Validation/package.nuspec -Version $Version -Verbosity detailed
+nuget pack ./src/X12.Sql/package.nuspec -Version $Version -Verbosity detailed
+nuget pack ./src/X12.Hipaa/package.nuspec -Version $Version -Verbosity detailed
 
+# Publish the packages
 nuget setApiKey $ApiKey -Source $Source -Verbosity detailed
-
 nuget push X12.*.nupkg -Source $Source
